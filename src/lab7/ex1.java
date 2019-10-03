11efb834992ecb2321d6eb3424b8db364e97c805
Chon <chonlada00>
2019-10-03 16:35:43 +0700

    lab6

---
+++ src/lab7/ex1.java
@@ -0,0 +1,23 @@
+package lab7;
+
+public class ex1 {
+    public static void main(String[] args) {
+        int x =100, y =20;
+
+
+        //if
+        if (x < y&& x==100){
+            //statements
+            System.out.println("x less than y.");
+        }else {
+            System.out.println("x mor than y.");
+        }
+
+
+
+
+    }//main
+
+
+
+}//class
